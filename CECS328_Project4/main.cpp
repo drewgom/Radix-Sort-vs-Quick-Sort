3a691a536054ad519d39fa5e094268be30f33c3f
Andrew Gomez <drew.gomez27@gmail.com>
2019-11-18 16:01:45 -0800

    Initial Commit

---
+++ CECS328_Project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CECS328_Project4
+//
+//  Created by Andrew Gomez on 11/18/19.
+//  Copyright © 2019 Andrew Gomez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
